17560387e6e82cae5f7468526b2f825825bf35ad
JrzenonDev <jrobertoonb@gmail.com>
2020-02-28 23:48:55 -0300

    chore: ajustando o componente Address, estilos e imagens e html

--- src/components/header/Address.js
+++ src/components/header/Address.js
@@ -5,7 +5,9 @@ export default class Address extends React.Component {
     render() {
         return(
             <div className="address">
-
+                <img src="/img/whatsapp.png" alt="whatsapp da empresa"/>
+                <strong> (xx) xxxxx - xxxx</strong> <br />
+                Endereço
             </div>
         );
     }
