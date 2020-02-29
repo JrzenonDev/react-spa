3c3543bc8218e4174bcacc4bd1cc15e0621415ad
JrzenonDev <jrobertoonb@gmail.com>
2020-02-29 11:36:44 -0300

    feat: component Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,16 @@ export default class Footer extends React.Component {
 
     render() {
         return(
-            null
+            <div className="footer">
+                <div>
+                    <img src="/img/logo-s.png" alt="Logo marca footer" />
+                </div>
+                <div>
+                    Endereço<br />
+                    Telefone<br />
+                    E-mail
+                </div>
+            </div>
         );
     }
 }
