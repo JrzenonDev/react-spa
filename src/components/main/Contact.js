0fd48e4e2c5d9525c149b62f6ba2ed8e0e04858a
JrzenonDev <jrobertoonb@gmail.com>
2020-03-01 23:29:03 -0300

    feat:  página contato com mask para telefone e estilos no form

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import InputMask from 'react-input-mask';
 
 export default class Contact extends React.Component {
 
     render() {
         return(
-            null
+            <div className="formulario">
+                <h1>Fale Conosco</h1>
+                <form name="form1" id="form1" action="/controllers/controllerForm.php" method="POST">
+                    <input type="text" name="nome" id="nome" placeholder="Nome:" />
+                    <InputMask type="tel" name="telefone" id="telefone" placeholder="Telefone:" mask="(99) 99999-9999" maskChar=" " />
+                    <input type="email" name="email" id="email" placeholder="E-mail:" />
+                    <textarea name="mensagem" id="mensagem" placeholder="Mensagem"></textarea>
+                    <input type="submit" value="Enviar" />
+                </form>
+            </div>
         );
     }
 }
