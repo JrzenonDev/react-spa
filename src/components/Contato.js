4ff0b74235c3af13d7cbaac766c9e8adbe12ac07
JrzenonDev <jrobertoonb@gmail.com>
2020-04-14 01:06:35 -0300

    chore: utilizando o react-router-dom

---
+++ src/components/Contato.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Contact from './main/Contact';
+
+export default class Contato extends React.Component{
+
+    render() {
+        return(
+            <div>
+                <Header />
+                <Contact></Contact>
+            </div>
+        );
+    }
+
+}
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import {Link} from 'react-router-dom';
 
 export default class Nav extends React.Component {
 
     render() {
         return(
             <div className="nav">
-                <AnchorLink href='#home'>Home</AnchorLink>
-                <AnchorLink href='#mapa'>Mapa</AnchorLink>
-                <AnchorLink href='#information'>Informação</AnchorLink>
-                <AnchorLink href='#contact'>Contato</AnchorLink>
+                <Link to="/">Home</Link>
+                <Link to="/contato">Contato</Link>
             </div>
         );
     }
